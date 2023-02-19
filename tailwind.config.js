f372ab8760971d2ceabb52dfa7104ba2418c433e
nunutech40 <49187517+nunutech40@users.noreply.github.com>
2023-02-19 14:54:38 +0700

    update add setup for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
   theme: {
     extend: {},
   },
