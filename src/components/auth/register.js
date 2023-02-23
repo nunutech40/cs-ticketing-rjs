61fefd445dab6fced3e3a93a582a7597ebba72cc
nunutech40 <49187517+nunutech40@users.noreply.github.com>
2023-02-23 15:55:32 +0700

    reframe login

--- src/components/auth/register.js
+++ src/components/auth/register.js
@@ -1,10 +1,7 @@
 import React, {Fragment, useContext, useEffect, useState} from 'react'
 import { useNavigate } from 'react-router-dom';
 import BugReportLogo from "../../assets/bugreport.png";
-
 const api = 'http://127.0.0.1:8000/api/';
-
-
 
 export default function Register(props) {
 
@@ -26,40 +23,40 @@ export default function Register(props) {
                 </div>
                 <div className="flex md:w-1/3 justify-left items-center">
                     <form className="bg-white  w-full">
-                        <h1 className="text-gray-800 font-bold text-2xl mb-6">Sing Up</h1>
+                        <h1 className="text-gray-800 font-bold text-2xl mb-6">Sign Up</h1>
                         <p className="text-sm font-normal text-gray-600 mb-6">Create your Bug Report Account</p>
 
                         <div className="mb-4 max-w-sm">
                             <label className="block text-grey-darker text-sm mb-2" htmlFor="fullness">
                             Full Name
                         </label>
-                            <input className="w-full py-2 px-3 text-grey-darker rounded-xl border-2"
+                            <input className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                    id="username" type="text" placeholder="Full Name"/>
                         </div>
                         <div className="mb-4 max-w-sm">
                             <label className="block text-grey-darker text-sm mb-2" htmlFor="fullness">
                                 Email
                             </label>
-                            <input className="w-full py-2 px-3 text-grey-darker rounded-xl border-2"
+                            <input className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                    id="email" type="email" placeholder="@gmail"/>
                         </div>
                         <div className="mb-4 max-w-sm">
                             <label className="block text-grey-darker text-sm mb-2" htmlFor="fullness">
                                 Password
                             </label>
-                            <input className="w-full py-2 px-3 text-grey-darker rounded-xl border-2"
+                            <input className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                    id="email" type="password" placeholder="Password"/>
                         </div>
                         <div className="mb-6 max-w-sm">
                             <label className="block text-grey-darker text-sm mb-2" htmlFor="fullness">
                                 Correct Password
                             </label>
-                            <input className="w-full py-2 px-3 text-grey-darker rounded-xl border-2"
+                            <input className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                    id="email" type="password" placeholder="Password"/>
                         </div>
                         <div className="mb-4 max-w-sm">
                             <button type="submit"
-                                    className="block w-full bg-sky-500 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Create Account
+                                    className="bg-sky-500 inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3">Create Account
                             </button>
                         </div>
 
