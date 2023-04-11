8f2c4cd52731384c7e6e241b8c146b137f559c8f
nunutech40 <49187517+nunutech40@users.noreply.github.com>
2023-04-11 21:25:51 +0700

    fix ui righsidebar

--- src/template/sidebar/rightsidebar.js
+++ src/template/sidebar/rightsidebar.js
@@ -1,29 +1,66 @@
-import React from "react";
+import React, { useState } from 'react'
+import Avatar from "../../assets/avatar.svg";
+import ICClose from "../../assets/close-circle.svg";
+import { icons } from "react-icons";
 
 export default function RightSidebar({ isSidebarVisible, toggleSidebar, userProfile, onSignoutClick }) {
 
+    const selectRole = ["CS", "TECH"];
+
+    const [formData, setFormData] = useState({
+        fullname: userProfile.name,
+        role: userProfile.role,
+        photoprofile: "",
+    });
+
     return (
         isSidebarVisible && (
-            <div className="fixed inset-y-0 right-0 w-64 h-screen bg-white transition-all duration-300 ease-in-out z-50 shadow-xl">
+            <div className="fixed inset-y-0 right-0 w-80 h-screen bg-white transition-all duration-300 ease-in-out z-50 shadow-xl">
                 {/* Sidebar content */}
                 <div className="flex flex-col h-full p-4">
-                    <h1 className="text-xl font-bold">Sidebar</h1>
-                    {/* Add your sidebar items here */}
-                    <div className="flex items-center my-4">
-                        <img className="h-20 w-20 rounded-full mr-4" src={userProfile.photoURL} alt="Profile" />
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Upload
-                        </button>
+                    <div className="flex flex-col items-center mt-8 my-4">
+                        <div className="w-full justify-start ml-8">
+                            <label htmlFor="position" className="block text-gray-700 mb-4">Profile</label>
+                        </div>
+                        <div className="flex items-center justify-center w-full">
+                            <div className="flex items-center w-full">
+                                <img className="h-16 w-16 ml-4 mr-4" src={Avatar} alt="logo" />
+                                <button className="bg-blue-500 hover:bg-blue-700 w-full mr-4 rounded-xl text-white font-bold py-2 px-4 rounded">
+                                    Upload Profile
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="mb-4 max-w-sm">
+                        <label className="block text-grey-darker text-sm mb-2" htmlFor="username">
+                            Name
+                        </label>
+                        <input
+                            type="text"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            id="fullname"
+                            placeholder="Full Name"
+                            name="fullname"
+                            value={userProfile.name}
+                            onChange={(e) => setFormData({ ...formData, fullname: e.target.value })}
+                        />
                     </div>
                     <div className="my-4">
-                        <label htmlFor="name" className="block text-gray-700 font-bold mb-2">Name:</label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" value={userProfile.name} />
-                    </div>
-                    <div className="my-4">
-                        <label htmlFor="position" className="block text-gray-700 font-bold mb-2">Position:</label>
-                        <select className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="position" value={userProfile.position}>
-                            <option value="CS">CS</option>
-                            <option value="TECH">TECH</option>
+                        <label className="block text-grey-darker text-sm mb-2" htmlFor="username">
+                            Position
+                        </label>
+                        <select
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            id="position"
+                            defaultValue={userProfile.role}
+                        >
+                            {selectRole.map((role, index) => (
+                                <option key={index} value={role.toUpperCase()}>
+                                    {role}
+                                </option>
+                            ))}
                         </select>
                     </div>
 
@@ -39,7 +76,7 @@ export default function RightSidebar({ isSidebarVisible, toggleSidebar, userProf
                     onClick={toggleSidebar}
                     className="absolute top-0 right-0 mt-4 mr-4 rounded-2xl text-gray-500 hover:text-gray-700"
                 >
-                    X
+                    <img src={ICClose} alt="logo" />
                 </button>
             </div>
         )
